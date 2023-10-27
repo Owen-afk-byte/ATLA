633c5b4dbbdf562410bb88a67601d74abc1fdc17
Owen-afk-byte <74568034+Owen-afk-byte@users.noreply.github.com>
2023-10-27 10:46:06 -0500

    we just found the average for all directors imdb ratings innit

--- data_exploration.py
+++ data_exploration.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 script = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-11/avatar.csv')
 
-print(script.head())
+# print first row
+print(script.iloc[0])
+
+# find best director
+print(script[['director', 'imdb_rating']].groupby(['director']).mean())
