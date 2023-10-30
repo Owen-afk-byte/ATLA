97cc7fdd9d07394724f4ff0344668540c5bd1a2b
Owen-afk-byte <74568034+Owen-afk-byte@users.noreply.github.com>
2023-10-30 10:56:09 -0500

    added count of the number of chapters that each character appears in

--- data_exploration.py
+++ data_exploration.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 script = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-11/avatar.csv')
 
+
+# ******************************************************************************
+
 # filter database for character_words entries that are not null
 character_scripts = script[script['character_words'].notnull()]
 
@@ -22,3 +25,27 @@ print(director_df)
 director_df['count'] = count.values
 
 print(director_df.sort_values(by='imdb_rating', ascending=False))
+
+# ******************************************************************************
+
+# filter database to have each character in each chapter
+script_characters = script[['chapter', 'character']].drop_duplicates(subset=['chapter', 'character'])
+
+# filter to remove 'Scene Description' from characters
+just_characters = script_characters.loc[script_characters["character"] != 'Scene Description' ]
+
+# should remove instances where 2 characters speak
+# for example "Katara and Sokka" is a value in the character column
+
+# count the number of chapters each character shows up in
+count_characters = just_characters.groupby('character').count()
+
+# rename the count column to something more appropriate
+count_characters = count_characters.rename({'chapter': 'no. of chapters'}, axis='columns')
+
+# sort the count
+sorted_count = count_characters.sort_values(by='no. of chapters', ascending=False)
+
+print(sorted_count)
+
+# ******************************************************************************
